f18ce6b4d898319f8041ea2730dbc7cfcf58df9b
suim.park <suim.park@duke.edu>
2023-09-09 00:44:11 -0400

    Edit python files

--- test_main.py
+++ test_main.py
@@ -1,18 +1,11 @@
 # Test main.py
 
-from main import DataShow
-
-GDP_data = {
-    "year": [2021, 2022, 2023],
-    "GDP rate": [2.8, 3.1, 3.0],
-    "GDP": ["1.637M", "1.73M", "1.83M"],
-}
+from main import GDP
 
 
-def test_DataShow():
-    result = DataShow(GDP_data)
-    assert result.loc[0, "GDP rate"] == 2.8
+def test_GDP():
+    assert GDP.loc[0, "GDP rate"] == 2.8
 
 
 if __name__ == "__main__":
-    test_DataShow()
+    test_GDP()
