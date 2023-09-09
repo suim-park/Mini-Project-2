a4107fb4bb99b7e09acd8fcdfabaa0298623baf0
suim.park <suim.park@duke.edu>
2023-09-09 01:21:41 -0400

    Edit code

--- test_main.py
+++ test_main.py
@@ -2,10 +2,15 @@
 
 from main import GDP
 
+GDP_data = {
+    "year": [2021, 2022, 2023],
+    "GDP rate": [2.8, 3.1, 3.0],
+    "GDP": ["1.637M", "1.73M", "1.83M"],
+}
+
 
 def test_GDP():
-    data_file = "GDP_data.csv"
-    result = GDP(data_file)
+    result = GDP(GDP_data)
 
     max_GDP_rate = max(result.iloc[:, 1])
     assert max_GDP_rate == 3.1
