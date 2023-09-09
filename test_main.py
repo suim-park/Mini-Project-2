2068aa008537e5f596fe8ba0cc0ae8639848e9f0
suim.park <suim.park@duke.edu>
2023-09-09 01:14:42 -0400

    Edit code

--- test_main.py
+++ test_main.py
@@ -7,7 +7,7 @@ def test_GDP():
     data_file = "GDP_data.csv"
     result = GDP(data_file)
 
-    max_GDP_rate = max(result.iloc[:, 0])
+    max_GDP_rate = max(result.iloc[1, :])
     assert max_GDP_rate == 3.1
 
 
